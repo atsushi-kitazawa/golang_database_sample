97162fb2c39ac11131098424c28bb1586d8fcf23
atsushi-kitazawa <atsushi.kitazawa.1987@gmail.com>
2021-08-17 01:08:30 +0900

    implement transaction.

--- main.go
+++ main.go
@@ -20,32 +20,47 @@ func main() {
     db, err := sql.Open("postgres", "host=127.0.0.1 port=15432 user=postgres dbname=postgres sslmode=disable")
     if err != nil {
 	log.Fatalln("err=", err)
+	return
     }
 
     // select table
     rows, err := db.Query("SELECT * FROM t1")
     if err != nil {
 	log.Fatalln("err=", err)
+	return
     }
 
     for rows.Next() {
-	var t t1
+	t := t1{}
 	rows.Scan(&t.id, &t.name)
-	fmt.Println(t.id)
-	fmt.Println(t.name)
+	fmt.Println(t.id, t.name)
     }
 
     // insert table
     tableCount, err := db.Query("SELECT COUNT(*) FROM t1")
     if err != nil {
 	log.Fatalln("err=", err)
+	return
     }
-
     var cnt int
     tableCount.Next()
     tableCount.Scan(&cnt)
 
+    tx, err := db.Begin()
+    if err != nil {
+	log.Fatalln("err=", err)
+	return
+    }
     id := cnt + 1
     name := "aaa" + strconv.Itoa(id)
-    db.QueryRow("INSERT INTO t1 VALUES ($1, $2)", id, name)
+    _, err = tx.Exec("INSERT INTO t1 VALUES ($1, $2)", id, name)
+    if err != nil {
+	log.Fatalln("err=", err)
+	tx.Rollback()
+	return
+    }
+    if err = tx.Commit(); err != nil {
+	log.Fatalln("err=", err)
+	return
+    }
 }
